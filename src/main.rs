5fc7fbe06270ee49894bc7f98a2057b044efdfdb
Finkelman <finkelman@semcog.org>
2015-12-10 00:53:08 -0500

    regexp dose not do what we nead so roll it our selves!

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate ole32;
 extern crate user32;
 extern crate clipboard_win;
 extern crate rustc_serialize; //To write rust objects to json
-extern crate regex; // to clean text
 
 use std::ptr;
 use std::mem;
@@ -20,15 +19,62 @@ use hot_key::*;
 mod settings;
 use settings::*;
 
-use regex::Regex;
-
 fn clean_text(raw: &str) -> String {
-    let out = Regex::new(r"\s+").unwrap().replace_all(raw, " ");
-    let out = Regex::new(r"[_]{4,}").unwrap().replace_all(&out, "___");
-    let out = Regex::new(r"[-]{4,}").unwrap().replace_all(&out, "---");
-    let out = Regex::new(r"[~]{4,}").unwrap().replace_all(&out, "~~~");
-    let out = Regex::new(r"[=]{4,}").unwrap().replace_all(&out, "===");
+    let mut out = String::new();
+    let mut last_ch = '=';
+    let mut count = 0;
+    for ch in raw.chars() {
+        if ch.is_whitespace() {
+            if !last_ch.is_whitespace() {
+                count = 1;
+                last_ch = ' ';
+                out.push(' ');
+            }
+        } else if ch != last_ch {
+            count = 1;
+            last_ch = ch;
+            out.push(ch);
+        } else if count < 3 {
+            count += 1;
+            out.push(ch);
+        }
+    }
     out
+}
+
+#[test]
+fn one_word() {
+    assert_eq!(clean_text("Hello"), "Hello");
+}
+
+#[test]
+fn two_word() {
+    assert_eq!(clean_text("Hello world!"), "Hello world!");
+}
+
+#[test]
+fn two_word_with_new_line() {
+    assert_eq!(clean_text("Hello \t\n \t\r \t\r\n world!"), "Hello world!");
+}
+
+#[test]
+fn two_word_with_tabs() {
+    assert_eq!(clean_text("Hello\t\n\t\r\t\r\nworld!"), "Hello world!");
+}
+
+#[test]
+fn two_word_with_underscore() {
+    assert_eq!(clean_text("Hello _________ world!"), "Hello ___ world!");
+}
+
+#[test]
+fn two_word_with_dash() {
+    assert_eq!(clean_text("Hello ----------- world!"), "Hello --- world!");
+}
+
+#[test]
+fn two_word_with_equals() {
+    assert_eq!(clean_text("Hello =========== world!"), "Hello === world!");
 }
 
 fn main() {
