43335dcb544b87750a397a2b6d82eb36597a4b1e
Finkelman <finkelman@semcog.org>
2015-12-14 13:27:33 -0500

    move clean text to a new file

--- src/main.rs
+++ src/main.rs
@@ -19,63 +19,8 @@ use hot_key::*;
 mod settings;
 use settings::*;
 
-fn clean_text(raw: &str) -> String {
-    let mut out = String::new();
-    let mut last_ch = '=';
-    let mut count = 0;
-    for ch in raw.chars() {
-        if ch.is_whitespace() {
-            if !last_ch.is_whitespace() {
-                count = 1;
-                last_ch = ' ';
-                out.push(' ');
-            }
-        } else if ch != last_ch {
-            count = 1;
-            last_ch = ch;
-            out.push(ch);
-        } else if count < 3 {
-            count += 1;
-            out.push(ch);
-        }
-    }
-    out
-}
-
-#[test]
-fn one_word() {
-    assert_eq!(clean_text("Hello"), "Hello");
-}
-
-#[test]
-fn two_word() {
-    assert_eq!(clean_text("Hello world!"), "Hello world!");
-}
-
-#[test]
-fn two_word_with_new_line() {
-    assert_eq!(clean_text("Hello \t\n \t\r \t\r\n world!"), "Hello world!");
-}
-
-#[test]
-fn two_word_with_tabs() {
-    assert_eq!(clean_text("Hello\t\n\t\r\t\r\nworld!"), "Hello world!");
-}
-
-#[test]
-fn two_word_with_underscore() {
-    assert_eq!(clean_text("Hello _________ world!"), "Hello ___ world!");
-}
-
-#[test]
-fn two_word_with_dash() {
-    assert_eq!(clean_text("Hello ----------- world!"), "Hello --- world!");
-}
-
-#[test]
-fn two_word_with_equals() {
-    assert_eq!(clean_text("Hello =========== world!"), "Hello === world!");
-}
+mod clean_text;
+use clean_text::*;
 
 fn main() {
     let _com = Com::new();
