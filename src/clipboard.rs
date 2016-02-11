7f98f1febf7909d840b8c56a1b977285e32e175a
Finkelman <finkelman@semcog.org>
2016-02-11 15:42:12 -0500

    ns is a lot smaller than ms, but it almost worked

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -56,21 +56,21 @@ pub fn send_ctrl_c() {
     send_key_event(VK_CONTROL as u16, KEYEVENTF_KEYUP);
 }
 
-pub fn what_on_clipboard_seq_num(clip_num: u32, n: u32) -> bool {
+pub fn what_on_clipboard_seq_num(clip_num: u32, n: u64) -> bool {
     for i in 1..(n + 1) {
         if get_clipboard_seq_num().unwrap_or(clip_num) != clip_num {
             return true;
         }
-        thread::sleep(Duration::new(0, 500 * i));
+        thread::sleep(Duration::from_millis(i));
     }
     get_clipboard_seq_num().unwrap_or(clip_num) != clip_num
 }
 
-pub fn what_on_get_clipboard_string(n: u32) -> Result<String, clipboard_win::WindowsError> {
+pub fn what_on_get_clipboard_string(n: u64) -> Result<String, clipboard_win::WindowsError> {
     for i in 1..(n + 1) {
         match get_clipboard_string() {
             Ok(x) => return Ok(x),
-            Err(_) => thread::sleep(Duration::new(0, 500 * i)),
+            Err(_) => thread::sleep(Duration::from_millis(i)),
         }
     }
     get_clipboard_string()
@@ -78,14 +78,14 @@ pub fn what_on_get_clipboard_string(n: u32) -> Result<String, clipboard_win::Win
 
 pub fn get_text() -> Result<String, clipboard_win::WindowsError> {
     println!("geting text");
-    let old_clip = what_on_get_clipboard_string(7);
+    let old_clip = what_on_get_clipboard_string(25);
     let old_clip_num = get_clipboard_seq_num().expect("Lacks sufficient rights to access \
                                                        clipboard(WINSTA_ACCESSCLIPBOARD)");
     send_ctrl_c();
-    if !what_on_clipboard_seq_num(old_clip_num, 7) {
+    if !what_on_clipboard_seq_num(old_clip_num, 25) {
         return Err(clipboard_win::WindowsError::new(0));
     }
-    let new_clip = what_on_get_clipboard_string(7);
+    let new_clip = what_on_get_clipboard_string(25);
     if let Ok(clip) = old_clip {
         let _ = set_clipboard(&clip);
     }
