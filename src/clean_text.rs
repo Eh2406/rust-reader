186df74e37e60d1d75156e71fdf8e57ac4e27398
Finkelman <finkelman@semcog.org>
2016-04-17 11:56:18 -0400

    O(log(n)) invert_u8idx for clean_text

--- src/clean_text.rs
+++ src/clean_text.rs
@@ -63,33 +63,29 @@ pub fn clean_text_u8idx<T: AsRef<str>>(raw: T) -> Vec<(usize, usize)> {
 }
 
 pub fn invert_u8idx(idx_vec: &Vec<(usize, usize)>, idx: Range<usize>) -> Range<usize> {
-    // O(n)
-    let start = idx_vec.iter().skip_while(|&&(_, x)| x < idx.start).map(|&(x, _)| x).next();
-    let end = idx_vec.iter().rev().skip_while(|&&(_, x)| x > idx.end).map(|&(x, _)| x).next();
-    start.unwrap()..end.unwrap()
+    // O(log(n))
+    let mut start_idx = match idx_vec.binary_search_by(|&(_, x)| x.cmp(&idx.start)) {
+        Ok(x) => x,
+        Err(x) => x,
+    };
+    while start_idx > 0 && idx_vec[start_idx - 1].1 == idx.start {
+        start_idx -= 1;
+    }
+    let start = idx_vec[start_idx].0;
+    let mut end_idx = match idx_vec[start_idx..].binary_search_by(|&(_, x)| x.cmp(&idx.end)) {
+        Ok(x) => x,
+        Err(x) => x,
+    } + start_idx;
+    while end_idx < idx_vec.len() - 1 && idx_vec[end_idx + 1].1 == idx.end {
+        end_idx += 1;
+    }
+    let end = idx_vec[end_idx].0;
+    start..end
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::ops::Range;
-    use wide_string::*;
-
-    fn slow_invert_u8idx<T: AsRef<str>>(raw: T, idx: Range<usize>) -> Range<usize> {
-        // O(n^2)
-        let s = raw.as_ref();
-        let mut out = 0..s.len() + 1;
-        for x in s.indices_utf8() {
-            let l = clean_text(&s[..x]).len();
-            if l == idx.start {
-                out.start = x;
-            }
-            if l == idx.end {
-                out.end = x;
-            }
-        }
-        out
-    }
 
     #[test]
     fn one_word() {
@@ -99,11 +95,6 @@ mod tests {
     #[test]
     fn one_word_u8idx() {
         let text = "Hello";
-        assert_eq!(slow_invert_u8idx(text, 0..5), 0..5);
-        assert_eq!(slow_invert_u8idx(text, 0..4), 0..4);
-        assert_eq!(slow_invert_u8idx(text, 4..5), 4..5);
-        assert_eq!(slow_invert_u8idx(text, 3..4), 3..4);
-
         let vec_u8idx = clean_text_u8idx(text);
         println!("{:?}", vec_u8idx);
         assert_eq!(invert_u8idx(&vec_u8idx, 0..5), 0..5);
@@ -135,15 +126,12 @@ mod tests {
     #[test]
     fn two_word_with_tabs_u8idx() {
         let text = "Hello\t\n\t\r\t\r\nworld!";
-        assert_eq!(slow_invert_u8idx(text, 0..5), 0..5);
-        assert_eq!(slow_invert_u8idx(text, 3..5), 3..5);
-        assert_eq!(slow_invert_u8idx(text, 3..7), 3..13);
-        assert_eq!(slow_invert_u8idx(text, 3..8), 3..14);
-
         let vec_u8idx = clean_text_u8idx(text);
         println!("{:?}", vec_u8idx);
         assert_eq!(invert_u8idx(&vec_u8idx, 0..5), 0..5);
         assert_eq!(invert_u8idx(&vec_u8idx, 3..5), 3..5);
+        assert_eq!(invert_u8idx(&vec_u8idx, 5..6), 5..12);
+        assert_eq!(invert_u8idx(&vec_u8idx, 6..7), 6..13);
         assert_eq!(invert_u8idx(&vec_u8idx, 3..7), 3..13);
         assert_eq!(invert_u8idx(&vec_u8idx, 3..8), 3..14);
     }
@@ -156,15 +144,12 @@ mod tests {
     #[test]
     fn two_word_with_underscore_u8idx() {
         let text = "Hello _________ world!";
-        assert_eq!(slow_invert_u8idx(text, 0..5), 0..5);
-        assert_eq!(slow_invert_u8idx(text, 3..5), 3..5);
-        assert_eq!(slow_invert_u8idx(text, 8..12), 8..18);
-        assert_eq!(slow_invert_u8idx(text, 11..15), 17..21);
-
         let vec_u8idx = clean_text_u8idx(text);
         println!("{:?}", vec_u8idx);
         assert_eq!(invert_u8idx(&vec_u8idx, 0..5), 0..5);
         assert_eq!(invert_u8idx(&vec_u8idx, 3..5), 3..5);
+        assert_eq!(invert_u8idx(&vec_u8idx, 7..9), 7..15);
+        assert_eq!(invert_u8idx(&vec_u8idx, 9..10), 9..16);
         assert_eq!(invert_u8idx(&vec_u8idx, 8..12), 8..18);
         assert_eq!(invert_u8idx(&vec_u8idx, 11..15), 17..21);
     }
@@ -193,16 +178,13 @@ mod tests {
     #[test]
     fn two_word_with_longchar_u8idx() {
         let text = "Hello \u{1d565}\u{1d565}\u{1d565}\u{1d565}\u{1d565} world!";
-        assert_eq!(slow_invert_u8idx(text, 0..5), 0..5);
-        assert_eq!(slow_invert_u8idx(text, 3..5), 3..5);
-        assert_eq!(slow_invert_u8idx(text, 6..20), 6..28);
-        assert_eq!(slow_invert_u8idx(text, 14..24), 14..32);
-
         let vec_u8idx = clean_text_u8idx(text);
         println!("{:?}", vec_u8idx);
         assert_eq!(invert_u8idx(&vec_u8idx, 0..5), 0..5);
         assert_eq!(invert_u8idx(&vec_u8idx, 3..5), 3..5);
-        assert_eq!(invert_u8idx(&vec_u8idx, 6..20), 6..28);
+        assert_eq!(invert_u8idx(&vec_u8idx, 5..6), 5..6);
+        assert_eq!(invert_u8idx(&vec_u8idx, 6..18), 6..26);
+        assert_eq!(invert_u8idx(&vec_u8idx, 18..20), 18..28);
         assert_eq!(invert_u8idx(&vec_u8idx, 14..24), 14..32);
     }
 
@@ -217,16 +199,13 @@ mod tests {
     fn two_word_with_multichar_u8idx() {
         let text = "Hello \u{5d4}\u{5a2}\u{5d4}\u{5a2}\u{5d4}\u{5a2}\u{5d4}\u{5a2}\u{5d4}\u{5a2} \
                     world!";
-        assert_eq!(slow_invert_u8idx(text, 0..5), 0..5);
-        assert_eq!(slow_invert_u8idx(text, 3..5), 3..5);
-        assert_eq!(slow_invert_u8idx(text, 6..20), 6..28);
-        assert_eq!(slow_invert_u8idx(text, 14..22), 14..30);
-
         let vec_u8idx = clean_text_u8idx(text);
         println!("{:?}", vec_u8idx);
         assert_eq!(invert_u8idx(&vec_u8idx, 0..5), 0..5);
         assert_eq!(invert_u8idx(&vec_u8idx, 3..5), 3..5);
         assert_eq!(invert_u8idx(&vec_u8idx, 6..20), 6..28);
+        assert_eq!(invert_u8idx(&vec_u8idx, 18..19), 18..27);
+        assert_eq!(invert_u8idx(&vec_u8idx, 14..18), 14..26);
         assert_eq!(invert_u8idx(&vec_u8idx, 14..22), 14..30);
     }
 
