c273c4d21d03c9c885e1dc2fb4df18728230e934
stolenletters <stolenletters@yahoo.com>
2016-04-29 15:10:42 -0700

    added fn for hotkey modifiers and displayed hotkeys in debug window

--- src/hot_key.rs
+++ src/hot_key.rs
@@ -2,13 +2,32 @@ use user32;
 
 use std::ptr::null_mut;
 
+fn convert_modifiers(modifiers:u32)->String{
+    let mut out = String::new();
+    let mut modifiers = modifiers;
+    if modifiers >= 4 {
+        modifiers -= 4;
+        out.push_str("Sht");
+        }
+    if modifiers >= 2 {
+        modifiers -= 2;
+        out.push_str("Ctr");
+        }
+    if modifiers >= 1 {
+        modifiers -= 1;
+        out.push_str("Alt");
+        }
+out
+}
+
 pub struct HotKey {
     id: i32,
 }
 
 impl HotKey {
     pub fn new(modifiers: u32, vk: u32, id: i32) -> Option<HotKey> {
-        println!("new for HotKey");
+        use std::char;
+        println!("new for HotKey: {}{} {}",convert_modifiers(modifiers), char::from_u32(unsafe {user32::MapVirtualKeyW(vk,2)}).unwrap(), id);
         // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646309.aspx
         let hr = unsafe { user32::RegisterHotKey(null_mut(), id, modifiers, vk) };
         if hr == 0 {
