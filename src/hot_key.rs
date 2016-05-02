659bdfa63ad7af4c627eadd23b1ccdac627de068
Finkelman <finkelman@semcog.org>
2016-05-02 17:16:15 -0400

    Fmt and tests

--- src/hot_key.rs
+++ src/hot_key.rs
@@ -2,22 +2,43 @@ use user32;
 
 use std::ptr::null_mut;
 
-fn convert_modifiers(modifiers:u32)->String{
+fn convert_modifiers(modifiers: u32) -> String {
     let mut out = String::new();
-    let mut modifiers = modifiers;
-    if modifiers >= 4 {
-        modifiers -= 4;
+    if (modifiers & 4) > 0 {
         out.push_str("Sht");
-        }
-    if modifiers >= 2 {
-        modifiers -= 2;
+    }
+    if (modifiers & 2) > 0 {
         out.push_str("Ctr");
-        }
-    if modifiers >= 1 {
-        modifiers -= 1;
+    }
+    if (modifiers & 1) > 0 {
         out.push_str("Alt");
-        }
-out
+    }
+    out
+}
+
+#[test]
+fn test_modifiers_alt() {
+    assert_eq!(&convert_modifiers(1), "Alt");
+}
+
+#[test]
+fn test_modifiers_ctr() {
+    assert_eq!(&convert_modifiers(2), "Ctr");
+}
+
+#[test]
+fn test_modifiers_sht() {
+    assert_eq!(&convert_modifiers(4), "Sht");
+}
+
+#[test]
+fn test_modifiers_ctralt() {
+    assert_eq!(&convert_modifiers(3), "CtrAlt");
+}
+
+#[test]
+fn test_modifiers_ctraltsht() {
+    assert_eq!(&convert_modifiers(7), "ShtCtrAlt");
 }
 
 pub struct HotKey {
@@ -27,7 +48,10 @@ pub struct HotKey {
 impl HotKey {
     pub fn new(modifiers: u32, vk: u32, id: i32) -> Option<HotKey> {
         use std::char;
-        println!("new for HotKey: {}{} {}",convert_modifiers(modifiers), char::from_u32(unsafe {user32::MapVirtualKeyW(vk,2)}).unwrap(), id);
+        println!("new for HotKey: {}{} {}",
+                 convert_modifiers(modifiers),
+                 char::from_u32(unsafe { user32::MapVirtualKeyW(vk, 2) }).unwrap(),
+                 id);
         // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646309.aspx
         let hr = unsafe { user32::RegisterHotKey(null_mut(), id, modifiers, vk) };
         if hr == 0 {
