05eb8b17dd271549a3aa6803f1b0fb8715f6f5fa
Eh2406 <YeomanYaacov@gmail.com>
2018-07-27 18:05:04 -0400

    Use the icons for the program

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ extern crate winres;
 fn main() {
     let mut res = winres::WindowsResource::new();
     res.set_manifest_file("rust_reader.manifest");
+    res.set_icon("Reader2.ico");
     res.compile().unwrap();
 }
 
