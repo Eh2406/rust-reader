227d77010438bc36eab41f6e749786b6d294e5e2
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2023-10-21 08:54:07 -0400

    Replace winres with winresource
    Fixes #17

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 #[cfg(windows)]
-extern crate winres;
+extern crate winresource;
 
 #[cfg(windows)]
 fn main() {
-    let mut res = winres::WindowsResource::new();
+    let mut res = winresource::WindowsResource::new();
     res.set_manifest_file("rust_reader.manifest");
     res.set_icon("Reader2.ico");
     res.compile().unwrap();
